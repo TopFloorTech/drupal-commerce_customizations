449eaa7f4dcdd1392e96983e9840302b04f975c9
Ben McClure <ben.mcclure@gmail.com>
2017-02-19 14:08:16 -0600

    Initial version

---
+++ src/EventSubscriber/CartEventSubscriber.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\commerce_customizations\EventSubscriber;
+
+use Drupal\hook_event_dispatcher\Event\Form\FormAlterEvent;
+use Drupal\hook_event_dispatcher\Event\Form\FormBaseAlterEvent;
+use Drupal\hook_event_dispatcher\HookEventDispatcherEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class CartEventSubscriber
+ *
+ * @package Drupal\commerce_customizations\EventSubscriber
+ */
+class CartEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * @param \Drupal\hook_event_dispatcher\Event\Form\FormAlterEvent $event
+   */
+  public function alterAddToCartForm(FormAlterEvent $event) {
+    if (strpos($event->getFormId(), 'commerce_order_item_default_add_to_cart_') !== 0) {
+      return;
+    }
+
+    $form = $event->getForm();
+
+    if (!isset($form['purchased_entity']['widget'][0]['attributes'])) {
+      return;
+    }
+
+    $attributes = $form['purchased_entity']['widget'][0]['attributes'];
+
+    foreach ($attributes as $key => $element) {
+      if (strpos($key, 'attribute_') !== 0) {
+        continue;
+      }
+
+      if (!empty($element['#options'])) {
+        if (count($element['#options']) == 1) {
+          $values = array_values($element['#options']);
+
+          if ($values[0] == 'N/A' || $values[0] == 'Standard Version') {
+            $form['purchased_entity']['widget'][0]['attributes'][$key]['#access'] = FALSE;
+          }
+        }
+      }
+    }
+
+    $event->setForm($form);
+  }
+
+  /**
+   * @param \Drupal\hook_event_dispatcher\Event\Form\FormAlterEvent $event
+   */
+  public function alterCartForm(FormAlterEvent $event) {
+    if (strpos($event->getFormId(), 'views_form_commerce_cart_form_') !== 0) {
+      return;
+    }
+
+    $form = $event->getForm();
+
+    $buttons = [
+      'checkout' => 'CartCheckoutButton',
+      'convert' => 'CartConvertButton',
+      'submit' => 'UpdateCartButton',
+    ];
+
+    foreach ($buttons as $id => $class) {
+      if (empty($form['actions'][$id])) {
+        continue;
+      }
+
+      $form['actions'][$id]['#attributes']['class'][] = 'CheckoutButton-input';
+      $form['actions'][$id]['#prefix'] = '<span class="' . $class . '">';
+      $form['actions'][$id]['#suffix'] = '</span>';
+    }
+
+    if (isset($form['remove_button'])) {
+      foreach ($form['remove_button'] as $index => $button) {
+        if (!is_numeric($index)) {
+          continue;
+        }
+
+        $form['remove_button'][$index]['#attributes']['class'][] = 'CartRemoveButton-input';
+        $form['remove_button'][$index]['#prefix'] = '<span class="CartRemoveButton">';
+        $form['remove_button'][$index]['#suffix'] = '</span>';
+      }
+    }
+
+    $event->setForm($form);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  static function getSubscribedEvents() {
+    $events = [];
+
+    $events[HookEventDispatcherEvents::FORM_ALTER][] = ['alterAddToCartForm'];
+    $events[HookEventDispatcherEvents::FORM_ALTER][] = ['alterCartForm'];
+
+    return $events;
+  }
+
+}
---
+++ src/EventSubscriber/CheckoutEventSubscriber.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Drupal\commerce_customizations\EventSubscriber;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\hook_event_dispatcher\Event\Form\FormIdAlterEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class CheckoutEventSubscriber
+ *
+ * @package Drupal\commerce_customizations\EventSubscriber
+ */
+class CheckoutEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * @param \Drupal\hook_event_dispatcher\Event\Form\FormIdAlterEvent $event
+   */
+  public function alterCheckoutForm(FormIdAlterEvent $event) {
+    $form = $event->getForm();
+
+    if (isset($form['actions']['next'])) {
+      $form['actions']['next']['#attributes']['class'][] = 'CheckoutButton-input';
+      $form['actions']['next']['#prefix'] = '<span class="CheckoutButton">';
+      $form['actions']['next']['#suffix'] = '</span>';
+    }
+
+    if (isset($form['shipping_information']['recalculate_shipping'])) {
+      $form['shipping_information']['recalculate_shipping']['#value'] = t('Show My Shipping Options');
+    }
+
+    if (isset($form['payment_information']['add_payment_method'])) {
+      $form['payment_information']['add_payment_method']['#after_build'][] = [$this, 'processPaymentInformation'];
+    }
+
+    $event->setForm($form);
+  }
+
+  public function processPaymentInformation(array $element, FormStateInterface $form_state) {
+    if (isset($element['billing_information'])) {
+      $element['billing_information']['copy_from_shipping'] = [
+        '#type' => 'checkbox',
+        '#title' => t('My billing address is the same as my shipping address.'),
+        '#default_value' => TRUE,
+      ];
+    }
+
+    if (isset($element['payment_details'])) {
+      $element['payment_details']['#sorted'] = FALSE;
+      $element['payment_details']['number']['#prefix'] = $this->buildCreditCardFormIcons();
+
+      $element['payment_details']['security_code']['#weight'] = 0.002;
+      $element['payment_details']['expiration']['#weight'] = 0.003;
+    }
+
+    return $element;
+  }
+
+  protected function buildCreditCardFormIcons() {
+    $element = [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['credit-card-form__icons']],
+      'children' => []
+    ];
+
+    foreach (['visa', 'mastercard', 'discover', 'amex'] as $card) {
+      $classes = [
+        'credit-card-form__icon',
+        'credit-card-form__icon--' . $card,
+        'fa',
+        'fa-cc-' . $card,
+      ];
+
+      $element['children'][] = [
+        '#markup' => '<i class="' . implode(" ", $classes) . '"></i>',
+      ];
+    }
+
+    return render($element);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  static function getSubscribedEvents() {
+    return [
+      'hook_event_dispatcher.form_multistep_default.alter' => [
+        ['alterCheckoutForm'],
+      ],
+    ];
+  }
+
+}
