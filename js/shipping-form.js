6d857a233aa0457fe129fd11ea9a14121c6ddbd7
Ben McClure <ben.mcclure@gmail.com>
2017-03-25 22:59:50 -0500

    by bmcclure: Update JS

--- js/shipping-form.js
+++ js/shipping-form.js
@@ -5,7 +5,7 @@
     attach: function (context, settings) {
       if (context !== document) {
         $('input[name="shipping_information[shipments][0][shipping_method][0]"]:checked', context)
-          .trigger('click');
+          .trigger('change');
       }
     }
   };
