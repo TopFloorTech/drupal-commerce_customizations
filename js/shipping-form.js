2fed8c18a8f29ae8ee8e1334812f2178f127d4bb
Ben McClure <ben.mcclure@gmail.com>
2017-04-15 17:33:58 -0500

    by bmcclure: Fix JS calculation making the page jump around

--- js/shipping-form.js
+++ js/shipping-form.js
@@ -3,9 +3,14 @@
 
   Drupal.behaviors.shippingForm = {
     attach: function (context, settings) {
-      if (context !== document) {
-        $('input[name="shipping_information[shipments][0][shipping_method][0]"]:checked', context)
-          .trigger('change');
+      var $body = $('body');
+      if (!$body.hasClass('js-shipping-triggered')) {
+        var $method = $('input[name="shipping_information[shipments][0][shipping_method][0]"]:checked', context);
+
+        if ($method.length > 0) {
+          $method.trigger('click');
+          $body.addClass('js-shipping-triggered');
+        }
       }
     }
   };
--- src/CommerceShippingAjaxHelper.php
+++ src/CommerceShippingAjaxHelper.php
@@ -33,19 +33,55 @@ class CommerceShippingAjaxHelper {
     }
 
     $order = clone $order;
-    self::validateAndSubmitShippingInformation($order, $form, $form_state);
 
-    /** @var \Drupal\commerce_order\OrderRefreshInterface $order_refresh */
-    $order_refresh = \Drupal::service('commerce_order.order_refresh');
-    $order_refresh->refresh($order);
+    if (self::shippingMethodHasChanged($order, $form, $form_state)) {
+      self::validateAndSubmitShippingInformation($order, $form, $form_state);
 
-    $order->preSave(\Drupal::entityTypeManager()->getStorage('commerce_order'));
+      /** @var \Drupal\commerce_order\OrderRefreshInterface $order_refresh */
+      $order_refresh = \Drupal::service('commerce_order.order_refresh');
+      $order_refresh->refresh($order);
 
-    // Refresh the order totals
-    $response = new AjaxResponse();
-    $response->addCommand(self::getOrderSummaryCommand($order));
+      $order->preSave(\Drupal::entityTypeManager()->getStorage('commerce_order'));
 
-    return $response;
+      // Refresh the order totals
+      $response = new AjaxResponse();
+      $response->addCommand(self::getOrderSummaryCommand($order));
+
+      return $response;
+    }
+  }
+
+  public static function shippingMethodHasChanged(OrderInterface $order, array &$form, FormStateInterface $form_state) {
+    $pane_form = $form['shipping_information'];
+
+    // Save the modified shipments.
+    $shipping_methods = [];
+
+    foreach (Element::children($pane_form['shipments']) as $index) {
+      /** @var \Drupal\commerce_shipping\Entity\ShipmentInterface $shipment */
+      $shipment = $pane_form['shipments'][$index]['#shipment'];
+      $shipping_methods[] = $shipment->getShippingMethodId();
+    }
+
+    $storage = $form_state->getStorage();
+
+    $match = TRUE;
+
+    if (isset($storage['selected_shipping_methods'])) {
+      foreach ($storage['selected_shipping_methods'] as $index => $shipping_method) {
+        if ($shipping_methods[$index] != $shipping_method) {
+          $match = FALSE;
+          break;
+        }
+      }
+    }
+
+    if (!$match) {
+      $storage['selected_shipping_methods'] = $shipping_methods;
+      $form_state->setStorage($storage);
+    }
+
+    return $match;
   }
 
   public static function validateAndSubmitShippingInformation(OrderInterface $order, array &$form, FormStateInterface $form_state) {
