0ba103e47e2d309bc9c93fa52b060a26b1ea0485
Ben McClure <ben.mcclure@gmail.com>
2017-11-08 17:54:21 -0600

    by bmcclure: Refactor and fix address-form.js

--- js/address-form.js
+++ js/address-form.js
@@ -1,46 +1,30 @@
 (function ($) {
   'use strict';
 
+  var jobTitleFieldClasses = [
+    '.field--name-field-job-title',
+    '.Field--name-field-job-title'
+  ];
+
+  var familyNameClasses = [
+    'div.family-name',
+    'span.family-name',
+    '.form-item-shipping-information-shipping-profile-address-0-address-family-name',
+    '.form-item-payment-information-add-payment-method-billing-information-address-0-address-family-name'
+  ];
+
   Drupal.behaviors.commerceCustomizationsJobTitle = {
     attach: function (context, settings) {
-      // Move Job Title field when editing
-      // Guest checkout Shipping form
-      $('#edit-shipping-information-shipping-profile-field-job-title-wrapper', context)
-          .detach()
-          .insertAfter($('.FormItem--shipping-information-shipping-profile-address-0-address-family-name', context));
-      // Guest checkout Billing Form
-      $('#edit-payment-information-add-payment-method-billing-information-field-job-title-wrapper', context)
-          .detach()
-          .insertAfter($('.FormItem--payment-information-add-payment-method-billing-information-address-0-address-family-name', context));
+      $(jobTitleFieldClasses.join(', '), context).each(function () {
+        var parent = $(this).parent();
 
-      // Logged in Shipping form
-      var $loggedInShipping = $('[data-drupal-selector="edit-shipping-information"]', context);
-      $loggedInShipping.find($('.field--name-field-job-title', context))
-          .detach()
-          .insertAfter($loggedInShipping.find($('.FormItem--shipping-information-shipping-profile-address-0-address-family-name', context)));
+        $(this)
+            .detach()
+            .insertAfter($(familyNameClasses.join(', '), parent));
+      });
 
-      // Logged in Billing form
-      var $loggedInBilling = $('[data-drupal-selector="edit-payment-information-add-payment-method-billing-information"]', context);
-      $loggedInBilling.find($('.field--name-field-job-title', context))
-          .detach()
-          .insertAfter($loggedInBilling.find($('.FormItem--payment-information-add-payment-method-billing-information-address-0-address-family-name', context)));
-
-      // Move Job Title field
-      var $addressDisplayShipping = $('[data-drupal-selector="edit-shipping-information-shipping-profile-rendered-profile-0"]', context);
-      $addressDisplayShipping.find($('.Field--name-field-job-title', context))
-          .detach()
-          .insertAfter($addressDisplayShipping.find($('.family-name', context)));
-
-      var $addressDisplayBilling = $('[data-drupal-selector="edit-payment-information-add-payment-method-billing-information-rendered-profile-0"]', context);
-      $addressDisplayBilling.find($('.Field--name-field-job-title', context))
-          .detach()
-          .insertAfter($addressDisplayBilling.find($('.family-name', context)));
-      // Remove label
-      $('.Field--name-field-job-title .Field-label', context)
-          .remove();
-      // Remove br tag for better formatting
-      $('.Field--name-field-job-title + br', context)
-          .remove();
+      $('.Field--name-field-job-title .Field-label', context).remove();
+      $('.Field--name-field-job-title + br', context).remove();
     }
   };
 }(jQuery));
